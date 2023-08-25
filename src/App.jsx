6c2602d65fa32aa8d3ff03dc4ff9d013059ad81f
Harshtank-io <tankharsh67@gmail.com>
2023-08-26 02:15:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import Navbar from "./Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import About from "./pages/About";
+import Product from "./pages/product";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          {/* <Route path="/services" element={<Services />} /> */}
+          <Route path="/About" element={<About/>}/>
+          <Route path="/Product" element={<Product/>}/>
+          {/* <Route path="/sign-up" element={<SignUp />} /> */}
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../components/Footer";
+import HeroSection from "../components/HeroSection";
+import Cards from "../components/Cards";
+
+function Home() {
+  return (
+    <>
+       <HeroSection />
+       <Cards /> 
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default Home;
