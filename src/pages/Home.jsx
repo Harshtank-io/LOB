9734baef79eae4ccccc411ea14ea07d85654da2c
Harshtank-io <tankharsh67@gmail.com>
2023-09-24 10:18:49 +0530

    bugs fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import HeroSection from "../components/HeroSection";
 import Cards from "../components/Cards";
 import Info from "../components/Info";
-import Me from "../components/me";
+import Me from "../components/Me";
 import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
-       <HeroSection />
-       <Cards />
-       <Info/>
-       <Me/>
-       <Footer/>
+      <HeroSection />
+      <Cards />
+      <Info />
+      <Me />
+      <Footer />
     </>
   );
 }
